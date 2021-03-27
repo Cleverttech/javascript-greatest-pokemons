8696ead2164e92d2e475773e3c8a7e4174c379a7
Cleverttech <dzifatedeku@gmail.com>
2021-03-27 16:39:42 +0100

    create iteration 1

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,5 +1,13 @@
 // Iteration 1: All Fire Pokemons- get the array of all Fire type pokemons
 
+const getAllFirePokemons = (pokemons) => {
+  const arr = [];
+  pokemons.filter((p) => {
+    p.type.includes("Fire") ? arr.push(p) : "";
+  });
+  return arr;
+};
+
 // Iteration 2: Shortest Pokemon- find the `name` of the shortest pokemon
 
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
@@ -10,4 +18,4 @@
 
 // Iteration 6: Alphabetic Order - Order by name and print the first 20 names
 
-// Iteration 7: Strong pokemons - return an array of first 15 pokemons, that have just one `weakness`. If there are less that 15, return all of them 
+// Iteration 7: Strong pokemons - return an array of first 15 pokemons, that have just one `weakness`. If there are less that 15, return all of them
