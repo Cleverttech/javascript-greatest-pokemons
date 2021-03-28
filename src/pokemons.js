7c0ffdf4820a80e1c17b5994853dd0590118c4b8
Cleverttech <dzifatedeku@gmail.com>
2021-03-28 15:52:51 +0200

    create Iteration 2- incomplete

--- src/pokemons.js
+++ src/pokemons.js
@@ -3,12 +3,37 @@
 const getAllFirePokemons = (pokemons) => {
   const arr = [];
   pokemons.filter((p) => {
-    p.type.includes("Fire") ? arr.push(p) : "";
+    p.type.includes("Fire") ? arr.push(p) : false;
   });
   return arr;
 };
 
 // Iteration 2: Shortest Pokemon- find the `name` of the shortest pokemon
+
+let shortestPokemon = (pokemons) => {
+  if (pokemons.length == 0) return 0;
+  // let heightNums = [];
+
+  let shortest = pokemons
+    .map((el) => {
+      return Number(el.height.slice(0, 4));
+    })
+    .sort((a, b) => {
+      return a - b;
+    });
+
+  // shortest = `${shortest} m`;
+  // console.log(shortestStr);
+  console.log(shortest[0]);
+
+  // if (el.valueOf(height) === shortest) {
+  //   // return pokemons.name;
+  //   console.log(el.name);
+  // }
+
+  // console.log(shortest);
+  return shortest;
+};
 
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
 
